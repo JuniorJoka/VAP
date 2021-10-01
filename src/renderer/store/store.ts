ac1a2dd32d5454521ccc28707735bd7be90e6700
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-01 21:25:37 +0000

    Initialize redux store

---
+++ src/renderer/store/store.ts
@@ -0,0 +1,7 @@
+import { createStore, Store } from "redux";
+
+import { rootReducer, RootState } from "./reducers";
+
+const store: Store<RootState> = createStore(rootReducer);
+
+export default store;
