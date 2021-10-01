7fc7b18892b2717307b43ce7d66c0de9759ff0fa
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-01 22:24:17 +0000

    Add default style overwrite

---
+++ src/renderer/styles/globalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+*{
+    user-select: none;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+
+}`;
