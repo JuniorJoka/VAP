0c201dbc7c5e3aa40acaad087208aea4b0fc0bfc
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-02 22:58:54 +0000

    Add Toast Types

--- src/shared/types/vap.ts
+++ src/shared/types/vap.ts
@@ -1,4 +1,17 @@
+import { ToastActions } from "./action-types";
+
 export interface Action {
   type: string;
   payLoad: any;
+}
+
+export interface ToastsAction extends Action {
+  type: ToastActions;
+  payLoad: Toast;
+}
+
+export interface Toast {
+  _id: number;
+  title: string;
+  content: string;
 }
