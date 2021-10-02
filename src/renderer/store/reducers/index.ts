f3d546374a42c57f09b172a152d1072c7bb9b2f9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-02 23:39:04 +0000

    Add toasts reducer to main reducer

--- src/renderer/store/reducers/index.ts
+++ src/renderer/store/reducers/index.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import { toasts } from "./toasts";
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  toasts,
+});
