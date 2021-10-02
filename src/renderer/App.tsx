2077095014a2b7c1b2a95a4dbc496c06a3a5a5c7
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-02 22:07:23 +0000

    Initialize the app

---
+++ src/renderer/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+/*
+|----------------------------------------------------------------------------------------
+|The App
+|----------------------------------------------------------------------------------------
+*/
+
+export const Vap: React.FC = ({ children }) => {
+  return (
+    <>
+      {children}
+      {/* Footer Goes Here */}
+      {/* Toast Menu goes here */}
+      {/* SubTitle Menu goes here */}
+      {/* Player Aux goes here */}
+    </>
+  );
+};
