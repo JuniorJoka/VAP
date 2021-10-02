7f0dc5f0cfc693c4d6a2cc1225829f2d3d6e6def
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-02 23:40:08 +0000

    Add toasts action

---
+++ src/renderer/store/actions/ToastsAction.ts
@@ -0,0 +1,29 @@
+import { ToastActions } from "../../../shared/types/action-types";
+import store from "../store";
+
+// Remove toast before timeout
+export const remove = (_id: number): void => {
+  store.dispatch({
+    type: ToastActions,
+    payload: { _id, title: "", content: "" },
+  });
+};
+
+// Add a toast
+export const add = (
+  title: string,
+  content: string,
+  duration: number = 3000
+): void => {
+  const _id = Date.now();
+  const toast = { _id, title, content };
+
+  store.dispatch({
+    type: ToastActions.TOAST_ADD,
+    payload: toast,
+  });
+
+  window.setTimeout(() => {
+    remove(_id);
+  }, duration);
+};
