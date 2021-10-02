d791e13e90902df292b5219a46c19d619d1b2d3f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-02 23:16:33 +0000

    Add toasts reducer

---
+++ src/renderer/store/reducers/toasts.ts
@@ -0,0 +1,26 @@
+import { ToastActions } from "../../../shared/types/action-types";
+import { Toast, ToastsAction } from "../../../shared/types/vap";
+
+type ToastState = Toast[];
+const initialState: ToastState = [];
+
+export const toasts = (
+  state = initialState,
+  action: ToastsAction
+): ToastState => {
+  switch (action.type) {
+    case ToastActions.TOAST_ADD: {
+      const toasts = [...state, action.payLoad];
+      return toasts;
+    }
+
+    case ToastActions.TOAST_REMOVE: {
+      const toasts = state.filter((toast) => toast._id !== action.payLoad._id);
+      return toasts;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
