222e89eaf64ee1859900cc79ae00a8b98e9a4521
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-03 15:10:01 +0000

    Add toast styles

--- src/renderer/elements/ToastStyle.ts
+++ src/renderer/elements/ToastStyle.ts
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
-export const Toast = styled.div``;
+export const Toast = styled.div`
+  width: 288px;
+  height: 102px;
+  border-radius: 8px;
+  background-color: gray;
+  margin: 8px 0;
+  padding: 8px;
+`;
 
 export const Title = styled.div``;
 
