042211a6b3c3ed6ed4ab5399baacaef56e598658
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-03 13:35:55 +0000

    Add toast-menu styles

--- src/renderer/components/Toasts/ToastsStyle.ts
+++ src/renderer/components/Toasts/ToastsStyle.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const ToastMenu = styled.div``;
+export const ToastMenu = styled.div`
+  position: absolute;
+  right: 0;
+  width: 320px;
+  height: 100vh;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
