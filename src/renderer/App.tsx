9d02e07d51b7c01d6671dee3a232a69ce221fdca
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-12 17:31:05 +0000

    add footer to app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Wrapper } from "./AppStyle";
 import Toasts from "./components/Toasts";
+import { Footer } from "./components/Footer";
 
 /*
 |----------------------------------------------------------------------------------------
@@ -13,7 +14,7 @@ export const Vap: React.FC = ({ children }) => {
   return (
     <Wrapper>
       {children}
-      {/* Footer Goes Here */}
+      <Footer />
       <Toasts />
       {/* SubTitle Menu goes here */}
       {/* Player Aux goes here */}
