ba651047fd953bb1128c10dc304af82a9648f97c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-12 18:40:04 +0000

    absolutely position toasts

--- src/renderer/styles/globalStyle.ts
+++ src/renderer/styles/globalStyle.ts
@@ -8,4 +8,12 @@ export default createGlobalStyle`
     box-sizing: border-box;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
+}
+
+div#toast{
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
 }`;
