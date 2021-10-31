0c6d98c9ebcde332c427f5bbffaca9b1daa6096b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-10-31 20:14:56 +0000

    Initialize video src component

---
+++ src/renderer/components/VideoSource/VideoSourceStyles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.video`
+  width: 50vw;
+  position: absolute;
+  visibility: hidden;
+`;
---
+++ src/renderer/components/VideoSource/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Wrapper } from "./VideoSourceStyles";
+
+export const VideoSrc = () => {
+  return <Wrapper src="C:/Users/Junior/Desktop/Learn Three.js while building a 3D game with physics.mp4" autoPlay />;
+};
