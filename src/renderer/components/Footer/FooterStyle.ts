4cd33addbb0a65a6bc036c2c740b436968a50e55
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-05 18:27:06 +0000

    fix footer to base

--- src/renderer/components/Footer/FooterStyle.ts
+++ src/renderer/components/Footer/FooterStyle.ts
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   height: 82px;
   display: grid;
   place-items: center;
-  position: absolute;
+  position: fixed;
   width: 100%;
   bottom: 0;
   box-shadow: 0px -5px 20px 0px black;
