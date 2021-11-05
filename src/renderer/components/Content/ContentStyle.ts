d60f2697a8569be2a4ddf464030baa31316612a1
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-11-05 18:27:47 +0000

    add main component to Component

--- src/renderer/components/Content/ContentStyle.ts
+++ src/renderer/components/Content/ContentStyle.ts
@@ -3,18 +3,27 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   flex: 1;
   border: 1px solid black;
+  background-color: blue;
+`;
+
+export const Main = styled.div`
+  width: inherit;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: yellow;
 `;
 
 export const Header = styled.header`
   border: 1px solid black;
-  width: inherit;
+  width: 95%;
   margin: 10px;
   height: 40px;
 `;
 
 export const SubHead = styled.div`
   border: 1px solid black;
-  width: inherit;
+  width: 95%;
   margin: 10px;
   height: 40px;
   display: flex;
