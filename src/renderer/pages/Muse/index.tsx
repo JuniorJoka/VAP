91bd97f5b8803b3f4ce6cffd245d57996c28f7cc
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-07 22:06:24 +0000

    add bare bones to pages

---
+++ src/renderer/pages/Muse/index.tsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+
+export const MusePage = () => <p>Hello from Muse</p>;
---
+++ src/renderer/pages/Vid/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const VideoPage: React.FC = ({ children }) => {
+  return <p>Hello from Video</p>;
+};
