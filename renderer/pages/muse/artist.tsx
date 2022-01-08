4902bb7f7652bb4b56a5d65ab1d551d76b0a30d8
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-08 23:40:34 +0000

    initialize muse artist page

---
+++ renderer/pages/muse/artist.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/Layout/Muse";
+
+export const Artist = () => {
+  return (
+    <Layout>
+      <p>Artist</p>
+    </Layout>
+  );
+};
+
+export default Artist;
