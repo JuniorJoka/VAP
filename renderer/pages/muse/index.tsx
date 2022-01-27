5245cf71c94c8721dbce504ba2f2b713d302cfd5
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-27 23:12:14 +0000

    add media control area

--- renderer/pages/muse/index.tsx
+++ renderer/pages/muse/index.tsx
@@ -6,6 +6,7 @@ const Index = () => {
     <main className="h-screen overflow-hidden  flex">
       <SideBar />
       <Content />
+      <div className="absolute bottom-0 w-screen h-20 bg-white"></div>
     </main>
   );
 };
