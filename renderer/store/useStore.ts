d0682ce8b963e3e8d35aad5801e812b1525e6092
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-01 20:16:50 +0000

    create store state

--- renderer/store/useStore.ts
+++ renderer/store/useStore.ts
@@ -1,5 +1,11 @@
 import create from "zustand";
+import channel from "../../shared/lib/ipc-channels";
+import { MuseState } from "../lib/types";
 
-export default create((set) => ({
+export default create<MuseState>((set) => ({
   Muse: [],
+  populateMuse: async () => {
+    const Muse = await global.ipcRenderer.invoke(channel.muse.READY);
+    set({ Muse: Muse });
+  },
 }));
