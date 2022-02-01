c65482f607667470f88b20771b43c97d4b4bc1cf
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-01 20:24:43 +0000

    add side bar component

--- renderer/components/SideBar/index.tsx
+++ renderer/components/SideBar/index.tsx
@@ -1,17 +1,22 @@
+import { FaSearch } from "react-icons/fa";
+
 export const SideBar = () => {
   return (
-    <div className="border-r-[.5px] border-b-gray- px-4 space-y-4">
+    <div className="border-r-[.5px] border-b-gray- px-4 space-y-4 text-xs">
       <button className="flex items-center space-x-3">
-        <div className="w-5 h-5 bg-slate-500" />
-        <p>Search</p>
+        <div className="w-7 h-7 bg-slate-500 rounded-md grid place-items-center">
+          <FaSearch />
+        </div>
+        <p className="hidden md:inline">Search</p>
       </button>
       <button className="flex items-center space-x-3">
-        <div className="w-5 h-5 bg-slate-500" />
-        <p>My Muse</p>
+        <div className="w-7 h-7 bg-slate-500 rounded-md" />
+
+        <p className="hidden md:inline">My Muse</p>
       </button>
       <button className="flex items-center space-x-3">
-        <div className="w-5 h-5 bg-slate-500" />
-        <p>Now Playing</p>
+        <div className="w-7 h-7 bg-slate-500 rounded-md" />
+        <p className="hidden md:inline">Now Playing</p>
       </button>
     </div>
   );
