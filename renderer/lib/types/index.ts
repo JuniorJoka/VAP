5a27dd12f519652c7d7c20a0e05d5852a63adb0b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-01 21:11:48 +0000

    add properties to muse state

--- renderer/lib/types/index.ts
+++ renderer/lib/types/index.ts
@@ -1,6 +1,11 @@
 import { MuseMeta } from "../../../shared/types/moth";
 
+interface keyMap<T> {
+  [key: string]: T;
+}
 export interface MuseState {
   Muse: MuseMeta[];
+  MuseByAlbum: keyMap<MuseMeta>;
+  MuseByArtist: keyMap<MuseMeta>;
   populateMuse: VoidFunction;
 }
