1740628cae7c6eb55517063a9b72fa1bca97a4c8
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-02 19:05:14 +0000

    add by group by funcs

--- renderer/store/useMuseStore.ts
+++ renderer/store/useMuseStore.ts
@@ -1,11 +1,68 @@
 import create from "zustand";
 import channel from "../../shared/lib/ipc-channels";
-import { MuseState } from "../lib/types";
+import { MuseMapper, MuseState } from "../lib/types";
 
-export default create<MuseState>((set) => ({
+export default create<MuseState>((set, get) => ({
   Muse: [],
+
+  MuseByArtist: new Map(),
+
+  MuseByAlbum: new Map(),
+
   populateMuse: async () => {
-    const Muse = await global.ipcRenderer.invoke(channel.muse.READY);
-    set({ Muse: Muse });
+    const Muse = await (global.ipcRenderer as Electron.IpcRenderer).invoke(channel.muse.READY);
+    set({ Muse });
+  },
+
+  groupByArtist: () => {
+    const MuseByArtist: MuseMapper = new Map();
+    const allMuse = get().Muse;
+
+    allMuse.map((muse) => {
+      let initial = muse.artist[0].toUpperCase();
+      const isNonLetter = initial.toLowerCase() === initial.toUpperCase();
+      // replace non-character initials with a pound sign
+      if (isNonLetter) {
+        initial = "#";
+      }
+      // initialize non-existing keys to an empty array
+      if (!MuseByArtist.has(initial)) {
+        MuseByArtist.set(initial, new Map());
+      }
+
+      const existingData = MuseByArtist.get(initial);
+      const content = (existingData.get(muse.artist) || []).concat(muse);
+      existingData.set(muse.artist, content);
+
+      MuseByArtist.set(initial, existingData);
+    });
+
+    set({ MuseByArtist });
+  },
+
+  groupByAlbum: () => {
+    const MuseByAlbum: MuseMapper = new Map();
+    const allMuse = get().Muse;
+
+    allMuse.map((muse) => {
+      let initial = muse.album[0].toUpperCase();
+      const isNonLetter = initial.toLowerCase() === initial.toUpperCase();
+
+      if (isNonLetter) {
+        initial = "#";
+      }
+
+      if (!MuseByAlbum.has(initial)) {
+        MuseByAlbum.set(initial, new Map());
+      }
+
+      const existingData = MuseByAlbum.get(initial);
+      const content = (existingData.get(muse.album) || []).concat(muse);
+      existingData.set(muse.album, content);
+
+      MuseByAlbum.set(initial, existingData);
+    });
+
+    set({ MuseByAlbum });
   },
 }));
