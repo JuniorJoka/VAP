28032c262c8a1bd515138237c76f0541688fadbf
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-02 20:04:58 +0000

    render data

--- renderer/components/AlbumsView/index.tsx
+++ renderer/components/AlbumsView/index.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+import { MuseMapper, MuseState } from "../../lib/types";
+import useMuseStore from "../../store/useMuseStore";
+import { Section } from "../Section";
+
+const selector = (state: MuseState): [MuseMapper, VoidFunction] => [state.MuseByAlbum, state.groupByAlbum];
+
 export const AlbumsView = () => {
-  return <div>AlbumsView</div>;
+  const [data, func] = useMuseStore(selector);
+  useEffect(() => {
+    func();
+  }, []);
+
+  return (
+    <div>
+      {Array.from(data.keys())
+        .sort()
+        .map((key) => (
+          <Section head={key} key={key} data={data.get(key)} />
+        ))}
+    </div>
+  );
 };
--- renderer/components/ArtistsView/index.tsx
+++ renderer/components/ArtistsView/index.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+import { MuseMapper, MuseState } from "../../lib/types";
+import useMuseStore from "../../store/useMuseStore";
+import { Section } from "../Section";
+
+const selector = (state: MuseState): [MuseMapper, VoidFunction] => [state.MuseByArtist, state.groupByArtist];
+
 export const ArtistsView = () => {
-  return <div>ArtistsView</div>;
+  const [data, fn] = useMuseStore(selector);
+  useEffect(() => {
+    fn();
+  }, []);
+
+  return (
+    <div>
+      {Array.from(data.keys())
+        .sort()
+        .map((key) => (
+          <Section head={key} key={key} data={data.get(key)} />
+        ))}
+    </div>
+  );
 };
