edeefcd64d55c6c1d51b44dec92a7645de147a7a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-03 19:19:02 +0000

    add store state types

--- renderer/lib/types/index.ts
+++ renderer/lib/types/index.ts
@@ -2,11 +2,13 @@ import { MuseMeta } from "../../../shared/types/moth";
 
 export interface MuseState {
   Muse: MuseMeta[];
+  activeMuse: MuseMeta;
   MuseByAlbum: MuseMapper;
   MuseByArtist: MuseMapper;
   populateMuse: VoidFunction;
   groupByAlbum: VoidFunction;
   groupByArtist: VoidFunction;
+  setActiveMuse: (muse: MuseMeta) => void;
 }
 
 export type MuseMapper = Map<string, Map<string, MuseMeta[]>>;
