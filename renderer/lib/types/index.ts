2bf9721327108570ee6c2179a6d519766d7e7020
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-03 20:45:26 +0000

    add play state types

--- renderer/lib/types/index.ts
+++ renderer/lib/types/index.ts
@@ -1,14 +1,23 @@
 import { MuseMeta } from "../../../shared/types/moth";
+import { AudioPlayer } from "../audioPayer";
 
+export enum PlayState {
+  playing,
+  paused,
+}
 export interface MuseState {
   Muse: MuseMeta[];
   activeMuse: MuseMeta;
   MuseByAlbum: MuseMapper;
   MuseByArtist: MuseMapper;
+  player: AudioPlayer;
+  playState: PlayState;
   populateMuse: VoidFunction;
   groupByAlbum: VoidFunction;
   groupByArtist: VoidFunction;
   setActiveMuse: (muse: MuseMeta) => void;
+  initializePlayer: VoidFunction;
+  setPlayState: (playState: PlayState) => void;
 }
 
 export type MuseMapper = Map<string, Map<string, MuseMeta[]>>;
