76bd72ff89c75023b1e26733547c0d7c01a771d2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-05 20:18:14 +0000

    add new store state

--- renderer/store/useMuseStore.ts
+++ renderer/store/useMuseStore.ts
@@ -2,8 +2,8 @@ import create from "zustand";
 import channel from "../../shared/lib/ipc-channels";
 import { MuseMeta } from "../../shared/types/moth";
 import { AudioPlayer } from "../lib/audioPayer";
-import { MuseMapper, MuseState, PlayState } from "../lib/types";
-import { groupBy } from "./utils";
+import { MuseMapper, MuseState, PlayerActions, PlayState } from "../lib/types";
+import { Actions, groupBy } from "./utils";
 
 export default create<MuseState>((set, get) => ({
   Muse: [],
@@ -13,6 +13,9 @@ export default create<MuseState>((set, get) => ({
   MuseByAlbum: new Map(),
 
   activeMuse: undefined,
+  activeMuseIndex: undefined,
+
+  activePlayList: [],
 
   player: undefined,
 
@@ -33,9 +36,31 @@ export default create<MuseState>((set, get) => ({
     set({ MuseByAlbum });
   },
 
-  setActiveMuse: (muse: MuseMeta) => set({ activeMuse: muse }),
+  setActiveMuse: (muse: MuseMeta, index: number) => set({ activeMuse: muse, activeMuseIndex: index }),
 
   initializePlayer: () => set({ player: new AudioPlayer() }),
 
   setPlayState: (playState: PlayState) => set({ playState }),
+
+  setActivePlayList: (activePlayList: MuseMeta[]) => set({ activePlayList }),
+
+  emit: async (action: PlayerActions) => {
+    const activePlaylist = get().activePlayList;
+    const activeIndex = get().activeMuseIndex;
+
+    if (!activePlaylist) return;
+
+    if (action === PlayerActions.next) {
+      const nextActiveIndex = Actions.get.next(activePlaylist.length, activeIndex);
+
+      set({ activeMuse: activePlaylist[nextActiveIndex], activeMuseIndex: nextActiveIndex });
+    } else if (action === PlayerActions.previous) {
+      const nextActiveIndex = Actions.get.previous(activePlaylist.length, activeIndex);
+      console.log(nextActiveIndex);
+
+      set({ activeMuse: activePlaylist[nextActiveIndex], activeMuseIndex: nextActiveIndex });
+    } else if (action === PlayerActions.playPause) {
+      await Actions.playPause(get().player);
+    }
+  },
 }));
