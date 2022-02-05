efb16a61eb54c3b63e3ca74cf339cb6154370b5e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-05 20:20:58 +0000

    set active playlist on track selection

--- renderer/components/Track/index.tsx
+++ renderer/components/Track/index.tsx
@@ -1,8 +1,27 @@
 import { MuseMeta } from "../../../shared/types/moth";
+import { MuseState } from "../../lib/types";
+import useMuseStore from "../../store/useMuseStore";
 
-export const Track = ({ track }: { track: MuseMeta }) => {
+const selector = (
+  state: MuseState
+): [(track: MuseMeta, index: number) => void, (activePlaylist: MuseMeta[]) => void] => [
+  state.setActiveMuse,
+  state.setActivePlayList,
+];
+
+export const Track = ({ track, index, playlist }: { track: MuseMeta; index: number; playlist: MuseMeta[] }) => {
+  const [activateTrack, activatePlaylist] = useMuseStore(selector);
+
+  const activate = () => {
+    activateTrack(track, index);
+    activatePlaylist(playlist);
+  };
+
   return (
-    <div className="hover:bg-gray-100 rounded-lg p-3 pr-6 pl-6 grid grid-cols-2 cursor-pointer align hover:shadow-md">
+    <div
+      className="hover:bg-gray-100 rounded-lg p-3 pr-6 pl-6 grid grid-cols-2 cursor-pointer align hover:shadow-md"
+      onClick={activate}
+    >
       <div className="flex items-center space-x-3  w-64">
         <div className="w-10 h-10 bg-slate-500" />
         <div>
