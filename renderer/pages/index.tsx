261a68311e0525a88e365f224d236ebb955d6437
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-05 20:21:52 +0000

    add store initializers

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Content } from "../components/Content";
 import { Controls } from "../components/Controls";
 import { SideBar } from "../components/SideBar";
@@ -5,11 +6,16 @@ import { MuseState } from "../lib/types";
 import useMuseStore from "../store/useMuseStore";
 
 // population func selector
-const selector = (state: MuseState) => state.populateMuse;
+const selector = (state: MuseState) => [state.populateMuse, state.initializePlayer];
 
 const IndexPage = () => {
-  // add music data to store
-  useMuseStore(selector)();
+  const [populate, initialize] = useMuseStore(selector);
+  useEffect(() => {
+    // add music data to store
+    populate();
+    //set AudioPlayer
+    initialize();
+  }, []);
 
   return (
     <main className="h-screen overflow-hidden  flex">
