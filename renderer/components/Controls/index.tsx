efda39d885700d72b5b939093aa74f925ce5ddd2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-06 18:03:07 +0000

    render no controls on inactive muse

--- renderer/components/Controls/index.tsx
+++ renderer/components/Controls/index.tsx
@@ -7,35 +7,39 @@ export const Controls = () => {
 
   return (
     <>
-      {!active ? (
-        <DummyControls />
-      ) : (
-        <div className="absolute bottom-0 w-screen h-20 bg-white flex flex-row">
-          <div className="flex flex-row ">
-            <div className="h-16 w-16 bg-red-300" />
-            <div className="h-16 w-48">
-              <p className="w-48 text-ellipsis overflow-hidden whitespace-nowrap">{active.title}</p>
+      {active && (
+        <div className="absolute bottom-0 w-screen h-20  backdrop-blur-3xl flex flex-row items-center">
+          <div className="flex flex-row hover:bg-pink-500/10 ">
+            <div className="h-20 w-20 bg-red-300" />
+            <div className="h-16 w-64">
+              <p className="w-64 text-ellipsis overflow-hidden whitespace-nowrap">{active.title}</p>
               <p>{active.artist}</p>
             </div>
           </div>
-          <div className="flex-1">
-            <div className="bg-blue-400 flex flex-col ">
-              <div className="flex space-x-6 items-center">
+          <div className="flex-1 ">
+            <div className="flex flex-col  items-center h-20">
+              <div className="flex space-x-6 items-center mt-1">
                 <div className="h-6 w-6 bg-pink-500" />
-                <div className="h-6 w-6 bg-pink-500" onClick={() => emit(PlayerActions.previous)} />
                 <div
-                  className="h-12 w-12 bg-pink-500 rounded-full"
+                  className="h-6 w-6 bg-gradient-to-r from-pink-500 to-purple-400"
+                  onClick={() => emit(PlayerActions.previous)}
+                />
+                <div
+                  className="h-12 w-12 bg-purple-500 rounded-full"
                   onClick={async () => {
                     await emit(PlayerActions.playPause);
                   }}
                 />
-                <div className="h-6 w-6 bg-pink-500" onClick={() => emit(PlayerActions.next)} />
+                <div
+                  className="h-6 w-6 bg-gradient-to-r from-purple-500 to-pink-500"
+                  onClick={() => emit(PlayerActions.next)}
+                />
                 <div className="h-6 w-6 bg-pink-500" />
               </div>
-              <div className="h-6 w-10 bg-white" />
+              <div className="h-2 w-11/12 mt-3 bg-white bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-500 rounded-full" />
             </div>
           </div>
-          <div className="bg-white w-48" />
+          <div className="w-64" />
         </div>
       )}
     </>
