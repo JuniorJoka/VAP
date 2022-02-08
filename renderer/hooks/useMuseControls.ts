04cfddd1c6a14dcd69f03c9066645f4f2911a68d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-08 19:46:24 +0000

    control play state for next muse

--- renderer/hooks/useMuseControls.ts
+++ renderer/hooks/useMuseControls.ts
@@ -1,21 +1,26 @@
 import { useEffect } from "react";
 import { MuseMeta } from "../../shared/types/moth";
 import { AudioPlayer } from "../lib/audioPayer";
-import { MuseState, PlayerActions } from "../lib/types";
+import { MuseState, PlayerActions, PlayState } from "../lib/types";
 import useMuseStore from "../store/useMuseStore";
 
-const selector = (state: MuseState): [MuseMeta, AudioPlayer, (action: PlayerActions) => Promise<void>] => [
+const selector = (state: MuseState): [MuseMeta, AudioPlayer, (action: PlayerActions) => Promise<void>, PlayState] => [
   state.activeMuse,
   state.player,
   state.emit,
+  state.playState,
 ];
 
 export const useMuseControls = (): [MuseMeta, AudioPlayer, (action: PlayerActions) => Promise<void>] => {
-  const [active, player, emit] = useMuseStore(selector);
+  const [active, player, emit, playState] = useMuseStore(selector);
 
   useEffect(() => {
     if (active) {
       player.setSrc(active.source);
+    }
+
+    if (playState === PlayState.playing) {
+      emit(PlayerActions.playPause);
     }
   }, [active]);
 
