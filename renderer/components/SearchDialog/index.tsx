50986c1add29624c7bd53a9ab9578bb21d5f3ee0
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-02-13 19:22:57 +0000

    re-implement dialog component

--- renderer/components/SearchDialog/index.tsx
+++ renderer/components/SearchDialog/index.tsx
@@ -1,22 +1,41 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { ReactNode } from 'react';
+/* eslint-disable implicit-arrow-linebreak */
+import { useTransition, animated } from '@react-spring/web';
+import { FaSearch } from 'react-icons/fa';
+import useDialog from '../../hooks/useDialog';
+import { DialogType } from '../../lib/types';
 
-export default function SearchDialog({ children }: { children: ReactNode }) {
+export default function Dialog() {
+  const [{ open }, set] = useDialog(DialogType.search);
+  const transition = useTransition(open, {
+    from: { transform: 'translate3d(0,-10px,0)', opacity: 0 },
+    enter: { opacity: 1, transform: 'translate3d(0,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(0,10px,0)', pointerEvents: 'none' },
+    delay: 1,
+  });
+
   return (
-    <Dialog.Root>
-      <Dialog.Trigger className=" flex items-center space-x-3">{children}</Dialog.Trigger>
-
-      <Dialog.Portal className="">
-        <Dialog.Overlay />
-
-        <Dialog.Content className="absolute top-28 bg-slate-600 h-14 w-4/6 left-[50%] rounded-xl transform translate-x-[-50%]">
-          <Dialog.Title>Search Dialog</Dialog.Title>
-
-          <Dialog.Description />
-
-          <Dialog.Close>yh</Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <>
+      {transition(
+        (styles, item) =>
+          item && (
+            <animated.div
+              style={{ opacity: styles.opacity, pointerEvents: styles.pointerEvents }}
+              className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-slate-900 bg-opacity-20"
+            >
+              <animated.div className="fixed w-[32rem] h-24 top-14 ">
+                <div className="flex items-center bg-white rounded-md">
+                  <FaSearch className="absolute translate-x-2" onClick={() => set(!open)} />
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    className="w-full h-10 rounded-md pl-7 "
+                  />
+                </div>
+              </animated.div>
+            </animated.div>
+          ),
+      )}
+    </>
   );
 }
