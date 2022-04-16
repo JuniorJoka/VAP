078a9e3023bcac49c0f957f6be0c9b8c6bbc36f4
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-04-16 19:16:57 +0100

    configure storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../renderer/components/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+};
