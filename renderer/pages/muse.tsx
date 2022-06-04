6f3b007c6edc5b54a36cab18534dd385531eddb9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-04 14:36:01 +0000

    add muse page

---
+++ renderer/pages/muse.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import Content from '../components/Content';
+import Controls from '../components/Controls/Controls';
+import DetailDialog from '../components/DetailDialog';
+import SideBar from '../components/SideBar';
+import { MuseState } from '../lib/types';
+import useMuseStore from '../store/useMuseStore';
+
+// population func selector
+const selector = (state: MuseState) => [state.populateMuse, state.initializePlayer];
+
+function IndexPage() {
+  const [populate, initialize] = useMuseStore(selector);
+  useEffect(() => {
+    // add music data to store
+    populate();
+    // set AudioPlayer
+    initialize();
+  }, []);
+
+  return (
+    <main className="h-screen overflow-hidden  flex">
+      <SideBar />
+      <Content />
+      <Controls />
+      <DetailDialog />
+    </main>
+  );
+}
+
+export default IndexPage;
