4f4578708febdaece7d4a119ae592ee023bba622
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-04 14:40:53 +0000

    add side bar component

---
+++ renderer/components/SideBar/SideBar.stories.tsx
@@ -0,0 +1,10 @@
+import SideBar from '.';
+
+export default {
+  title: 'Component/SideBar',
+  component: SideBar,
+};
+
+const Template = (args) => <SideBar />;
+
+export const Default = Template.bind({});
--- renderer/components/SideBar/index.tsx
+++ renderer/components/SideBar/index.tsx
@@ -1,26 +1,77 @@
-import { FaSearch } from 'react-icons/fa';
+import {
+  BsCameraVideo,
+  BsGear,
+  BsHouse,
+  BsImage,
+  BsMusicNote,
+  BsSearch,
+  BsStack,
+} from 'react-icons/bs';
 import useDialog from '../../hooks/useDialog';
 import { DialogType } from '../../lib/types';
+import Link from 'next/link';
 
 export default function SideBar() {
   const [, set] = useDialog(DialogType.search);
   return (
-    <div className="border-r-[.5px] border-b-gray- px-4 space-y-4 text-xs">
-      <button className="flex items-center space-x-3" type="button" onClick={() => set(true)}>
-        <div className="w-7 h-7 bg-slate-500 rounded-md grid place-items-center">
-          <FaSearch />
-        </div>
-        <p className="hidden md:inline">Search</p>
-      </button>
-      <button className="flex items-center space-x-3" type="button">
-        <div className="w-7 h-7 bg-slate-500 rounded-md" />
-
-        <p className="hidden md:inline">My Muse</p>
-      </button>
-      <button className="flex items-center space-x-3" type="button">
-        <div className="w-7 h-7 bg-slate-500 rounded-md" />
-        <p className="hidden md:inline">Now Playing</p>
-      </button>
+    <div className="border-r-[.5px]  h-full w-14 bg-gray-100 flex flex-col items-center justify-between pt-4 pb-32">
+      <div>
+        <button
+          type="button"
+          className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          onClick={() => set(true)}
+        >
+          <BsSearch />
+        </button>
+        <Link href="/">
+          <button
+            type="button"
+            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          >
+            <BsHouse />
+          </button>
+        </Link>
+        <Link href="/muse">
+          <button
+            type="button"
+            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          >
+            <BsMusicNote />
+          </button>
+        </Link>
+        <Link href="/video">
+          <button
+            type="button"
+            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          >
+            <BsCameraVideo />
+          </button>
+        </Link>
+        <Link href="/image">
+          <button
+            type="button"
+            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          >
+            <BsImage />
+          </button>
+        </Link>
+        <Link href="/playlist">
+          <button
+            type="button"
+            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          >
+            <BsStack />
+          </button>
+        </Link>
+      </div>
+      <Link href="/settings">
+        <button
+          type="button"
+          className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded "
+        >
+          <BsGear />
+        </button>
+      </Link>
     </div>
   );
 }
