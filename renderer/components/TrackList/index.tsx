68959a00538c9633fd724a1af693fb39e045b21a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-04 14:41:09 +0000

    add tracklist component

--- renderer/components/TrackList/index.tsx
+++ renderer/components/TrackList/index.tsx
@@ -1,15 +1,20 @@
 import { MuseState } from '../../lib/types';
 import useMuseStore from '../../store/useMuseStore';
-import Track from '../Track';
+import Track from '../Track/Track';
 
 const selector = (state: MuseState) => state.Muse;
 
 export default function TrackList() {
   const tracks = useMuseStore(selector);
   return (
-    <div>
+    <div className="space-y-2">
       {tracks.map((track, index) => (
-        <Track key={track.title + track.duration} track={track} index={index} playlist={tracks} />
+        <Track
+          key={track.title + track.duration}
+          track={track}
+          even={index % 2 === 0}
+          // playlist={tracks}
+        />
       ))}
     </div>
   );
