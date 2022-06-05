24064c7b6f2f13260ead821f3f669e6abcca50bd
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-05 23:55:21 +0000

    add video channel

--- shared/lib/ipc-channels.ts
+++ shared/lib/ipc-channels.ts
@@ -1,7 +1,11 @@
 const channel = {
   muse: {
-    READY: "MUSE_READY",
-    NEW_CONTENT: "MUSE_NEW_CONTENT",
+    READY: 'MUSE_READY',
+    NEW_CONTENT: 'MUSE_NEW_CONTENT',
+  },
+  video: {
+    READY: 'VIDEO_READY',
+    NEW_CONTENT: 'VIDEO_NEW_CONTENT',
   },
 };
 
