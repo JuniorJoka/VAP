c3e4c3ef91769479a720c47ece18585a58504590
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-06-12 16:31:06 +0000

    map bar items to side items component

--- renderer/components/SideBar/index.tsx
+++ renderer/components/SideBar/index.tsx
@@ -10,68 +10,36 @@ import {
 import useDialog from '../../hooks/useDialog';
 import { DialogType } from '../../store/Dialog';
 import Link from 'next/link';
+import SideItem from '../SideItem';
+
+const barItems = [
+  { name: 'Home', icon: BsHouse, link: '/' },
+  { name: 'Music library', icon: BsMusicNote, link: '/muse' },
+  { name: 'Video library', icon: BsCameraVideo, link: '/video' },
+  { name: 'Image library', icon: BsImage, link: '/image' },
+  { name: 'Playlist', icon: BsStack, link: '/playlist' },
+];
 
 export default function SideBar() {
   const [, set] = useDialog(DialogType.search);
   return (
-    <div className="border-r-[.5px]  h-full w-14 bg-gray-100 flex flex-col items-center justify-between pt-4 pb-32">
+    <div className="border-r-[.5px]  h-full w-14 lg:w-44 xl:w-56 bg-gray-100 flex flex-col items-center justify-between pt-4 pb-32">
       <div>
         <button
           type="button"
-          className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
+          className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded lg:w-40 lg:justify-start p-3 xl:w-52"
           onClick={() => set(true)}
         >
-          <BsSearch />
+          <div className="flex space-x-4 items-center">
+            <BsSearch />
+            <p className="hidden lg:block">Search</p>
+          </div>
         </button>
-        <Link href="/">
-          <button
-            type="button"
-            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
-          >
-            <BsHouse />
-          </button>
-        </Link>
-        <Link href="/muse">
-          <button
-            type="button"
-            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
-          >
-            <BsMusicNote />
-          </button>
-        </Link>
-        <Link href="/video">
-          <button
-            type="button"
-            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
-          >
-            <BsCameraVideo />
-          </button>
-        </Link>
-        <Link href="/image">
-          <button
-            type="button"
-            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
-          >
-            <BsImage />
-          </button>
-        </Link>
-        <Link href="/playlist">
-          <button
-            type="button"
-            className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded"
-          >
-            <BsStack />
-          </button>
-        </Link>
+        {barItems.map(({ name, icon, link }) => (
+          <SideItem name={name} href={link} icon={icon} />
+        ))}
       </div>
-      <Link href="/settings">
-        <button
-          type="button"
-          className="w-10 h-10 hover:bg-gray-200 flex items-center justify-center rounded "
-        >
-          <BsGear />
-        </button>
-      </Link>
+      <SideItem name="Settings" href="/settings" icon={BsGear} />
     </div>
   );
 }
